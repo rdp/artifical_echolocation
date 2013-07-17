5c2de2982de1eba54d8a983e976832ab30e20660
rogerdpack <rogerpack2005@gmail.com>
2013-07-17 17:41:28 -0600

    yo

---
+++ all.bat
@@ -0,0 +1,6 @@
+call go2 -1 1
+call go2 -1 3
+call go2 0 2
+call go2 2 1
+call go2 2 3
+sox -m -1_1.wav -1_3.wav 0_2.wav 2_1.wav 2_3.wav all.wav
--- go2.bat
+++ go2.bat
@@ -1,5 +1,9 @@
+@rem also need to preced with  this: > sox XX.wav -b 16 XX.raw channels 1 rate 44100
+@rem also need to setup openal config first...
+
 @rem create spatial file of click, no z-axis for now
-footsteps.exe %1 %2 0
+footsteps.exe %1 %2 0 tick.raw
 @rem pad with initial silence
-sox output.wav output_longer.wav pad %2 0
-cp output_longer.wav %1_%2.wav
+call sox output.wav output_longer.wav pad %2 0
+call cp -- output_longer.wav %1_%2.wav
+echo created %1_%2.wav
