8c324573fa8f0421fe771c07c474415a1cee3884
rogerdpack <rogerpack2005@gmail.com>
2013-07-17 17:49:38 -0600

    eureka

--- all.bat
+++ all.bat
@@ -1,8 +1,10 @@
+rm *_*.wav
 call go2 -1 1
 mv -- -1_1.wav neg1_1.wav
-call go2 -1 3
-mv -- -1_3.wav neg1_3.wav
+call go2 -1 3.1
+mv -- -1_3.1.wav neg1_3.1.wav
 call go2 0 2
-call go2 2 1
+call go2 2 1.1
 call go2 2 3
-sox -m -- neg1_1.wav neg1_3.wav 0_2.wav 2_1.wav 2_3.wav all.wav
+call sox -m -- neg1_1.wav neg1_3.1.wav 0_2.wav 2_1.1.wav 2_3.wav all.wav
+call ffplay all.wav -autoexit
