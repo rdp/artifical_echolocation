4282a41792d94a9550f3dd8db22ee57cdda5adc8
rogerdpack <rogerpack2005@gmail.com>
2013-07-17 17:45:00 -0600

    the prototype lives LOL

--- all.bat
+++ all.bat
@@ -1,6 +1,8 @@
 call go2 -1 1
+mv -- -1_1.wav neg1_1.wav
 call go2 -1 3
+mv -- -1_3.wav neg1_3.wav
 call go2 0 2
 call go2 2 1
 call go2 2 3
-sox -m -1_1.wav -1_3.wav 0_2.wav 2_1.wav 2_3.wav all.wav
+sox -m -- neg1_1.wav neg1_3.wav 0_2.wav 2_1.wav 2_3.wav all.wav
