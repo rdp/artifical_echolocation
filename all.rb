eb9c31ebe4039d2d849dd790c7715d4ba0e48d04
rogerdpack <rogerpack2005@gmail.com>
2013-07-18 15:16:47 -0600

    regenerate some with the new 0 z axis from the .exe

--- all.rb
+++ all.rb
@@ -10,19 +10,20 @@ name="figure_x"
 
 for x, y in eval(name) do
   z = 0
-  system(c = "go2 #{x} #{y} #{z}")
-  puts c
-  output_filename="#{x}_#{y}_#{z}.wav"
   if x < 0
     x = "_#{x}" # command line friendlier name
   end
   if y < 0
     y = "_#{y}" # command line friendlier 
   end
-  desired_output_filename="#{x}_#{y}.wav"
-  FileUtils.mv output_filename, desired_output_filename
-  all << desired_output_filename
+  output_filename="#{x}_#{y}_#{z}.wav"
+  end_output_filename="#{x}_#{y}.wav"
+  if !File.exist? end_output_filename
+    system(c = "go2 #{x} #{y} #{z}")
+  end
+  FileUtils.mv output_filename, end_output_filename
+  all << end_output_filename
 end
 system(c = "sox -m #{all.join ' '} #{name}.wav")
-puts c
+puts "created #{name}.wav"
 system("ffplay #{name}.wav -autoexit")
